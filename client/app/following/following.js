064f626f59d581b376b579bbeb399536d09f0f6b
DONYARIT MEEBUNGPHRAO <donyarit_m@cmu.ac.th>
2019-05-19 22:42:21 +0700

    following / unfollowing fuctions

--- client/app/following/following.js
+++ client/app/following/following.js
@@ -7,9 +7,17 @@ angular.module('followingList', ['ngRoute'])
       var self = this;
 
       const requestOptions = {
-          headers: { 'X-session': $rootScope.x_session }
+          headers: { 'X-session': $rootScope.x_session } //ref 600611006
       };
-
+        self.sendFollow = function sendFollow(followingname) {
+            const data = "followingname=" + encodeURIComponent(followingname);
+            $http.post('http://localhost:8080/twitterapi/following/', data, requestOptions);
+        }
+        self.sendUnFollow = function sendUnFollow(followingname) {
+            $http.defaults.headers.delete = { 'X-session': $rootScope.x_session };
+            const data = "followingname=" + encodeURIComponent(followingname);
+            $http.delete('http://localhost:8080/twitterapi/following/?' + data);
+        }
       $http.get('http://localhost:8080/twitterapi/following/', requestOptions).then(function (response) {
         self.followings = response.data;
       });
